3d8a1f77110bb083693ec1bed21176c7c93b6dc5
Konstantin <phpconf@gmail.com>
2016-02-24 13:58:51 +0300

    fix import

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='url_paginator',
-    version='0.0.1',
+    version='0.0.2',
     packages=['url_paginator'],
     url='https://github.com/Frozen/url_paginator',
     license='MIT',
---
+++ url_paginator/__init__.py
@@ -0,0 +1,1 @@
+from .url_paginator import UrlPage, UrlPaginator
